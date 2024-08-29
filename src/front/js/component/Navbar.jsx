2bafbc323616056a9a1a1c734b4487eb8867f130
Miquel Carnot <64230328+greyzeids@users.noreply.github.com>
2024-08-29 22:57:04 +0000

    Changed classes to Bootstrap for better responsiveness and adapted CSS

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -7,30 +7,42 @@ import "../../styles/index.css";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-center">
-                    <Link className="navbar-brand ms-3" to="/">
-                        <img
-                            src={logo}
-                            width="76.64"
-                            height="67.52"
-                            className="d-inline-block align-text-top rotate-vertical"
-                        />
-                    </Link>
-                    <Link className="ms-3 text-decoration-none" to="/">
-                        <span className="navbar-text">My Game Guru</span>
-                    </Link>
-                </div>
-                <div className="d-flex align-items-center">
-                    <Link className="ms-3 text-decoration-none" to="/favorites">
-                        <span className="navbar-text">Favoritos</span>
-                    </Link>
-                    <Link className="text-decoration-none ms-3" to="/login">
-                        <span className="navbar-text">Mis valoraciones</span>
-                    </Link>
-                    <Dropdown />
-                    <ThemeSwitcher />
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid d-flex align-items-center">
+                <Link className="navbar-brand d-flex align-items-center" to="/">
+                    <img
+                        src={logo}
+                        width="76.64"
+                        height="67.52"
+                        className="d-inline-block align-text-top rotate-vertical"
+                    />
+                </Link>
+                <Link
+                    className="ms-3 text-decoration-none d-flex align-items-center"
+                    to="/"
+                >
+                    <span className="navbar-text">My Game Guru</span>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="dropdown-arrow">&#9660;</span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto d-flex align-items-center">
+                        <li className="nav-item d-flex align-items-center">
+                            <Link
+                                className="nav-link d-flex align-items-center"
+                                to="/favorites"
+                            >
+                                <span className="navbar-text">Favoritos</span>
+                            </Link>
+                        </li>
+                        <li className="nav-item d-flex align-items-center">
+                            <Dropdown />
+                        </li>
+                        <li className="nav-item d-flex align-items-center">
+                            <ThemeSwitcher />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
